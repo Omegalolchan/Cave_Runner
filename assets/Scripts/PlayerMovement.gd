9846fb4d2c23132e7e5d147071377539d11aeaae
Pedro Lucas <pedrolucasrochap+git@gmail.com>
2024-02-27 23:56:12 -0300

    Tudo configurado pro slide

--- assets/Scripts/PlayerMovement.gd
+++ assets/Scripts/PlayerMovement.gd
@@ -2,13 +2,17 @@ extends CharacterBody2D
 
 const WALK_SPEED = 40.0
 var direction : float
+
 var jump_input : bool
+var slide_input : bool
+
 var is_jumping
+var is_sliding
 var current_jump_time = 0.0
 
-@export var jump_max_time = 1
-@export var jump_speed = -100.0
-@export var gravity_scale : float = 8 
+@export var jump_max_time = 15
+@export var jump_speed = -30.0
+@export var gravity_scale : float = 10
 
 var gravity = 8 * gravity_scale
 
@@ -23,11 +27,21 @@ func JumpInputRelease():
 	jump_input = false
 	return
 
-func _physics_process(delta):
+func GetInput():
 	direction = Input.get_axis("moveLeft", "moveRight")
 	if(Input.is_action_just_pressed("jump")): JumpInputPress()
 	if(Input.is_action_just_released("jump")): JumpInputRelease()
 	
+	if Input.is_action_pressed("slide"):
+		slide_input = true 
+	else:
+		slide_input = false
+	
+	return
+
+func _physics_process(delta):
+	GetInput()
+	#region Jump
 	if (jump_input) :
 		current_jump_time -= 1
 	else: 
@@ -35,16 +49,24 @@ func _physics_process(delta):
 	
 	if(current_jump_time <= 0):
 		is_jumping = false
-	
-	# Add the gravity.
-	if !is_on_floor() && !is_jumping:
-		velocity.y += gravity * delta
-	
+
 	if(current_jump_time > 0) :
 		velocity.y = jump_speed
+	#endregion
 	
+	if !is_on_floor():
+		velocity.y += gravity * delta
+
+	if is_on_floor() && slide_input:
+		is_sliding = true
+		Slide()
+
 	velocity.x = direction * WALK_SPEED
-	
+
 	move_and_slide()
-	
+	var collision = get_last_slide_collision()
+	return
+
+func Slide():
+	# TO DO : Slide 
 	return
