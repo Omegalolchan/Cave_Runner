1398bc12e6b8e638746b6ccac0f19274fef08324
Pedro Lucas <pedrolucasrochap+git@gmail.com>
2024-03-02 09:27:38 -0300

    Fixed velocity angle based on ground

--- assets/Scripts/PlayerMovement.gd
+++ assets/Scripts/PlayerMovement.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const WALK_SPEED = 40.0
+const WALK_SPEED = 40.0 / 60
+const FPS_DELTA = 0.016
 var direction : float
 
 var jump_input : bool
@@ -8,17 +9,21 @@ var slide_input : bool
 
 var is_jumping
 var is_sliding
-var current_jump_time = 0.0
+var on_floor
+var current_jump_time : float
+var current_coyote_time : float
 
+@export var coyote_max_time = 4
 @export var jump_max_time = 15
-@export var jump_speed = -30.0
+@export var jump_speed = -40.0
 @export var gravity_scale : float = 10
 
-var gravity = 8 * gravity_scale
+var gravity = (8 * 0.016) * gravity_scale
 
 func JumpInputPress():
 	jump_input = true
-	if on_floor():
+	current_coyote_time = 0
+	if on_floor:
 		current_jump_time = jump_max_time
 		is_jumping = true
 	return
@@ -51,34 +56,55 @@ func _physics_process(delta):
 		is_jumping = false
 
 	if(is_jumping) :
-		velocity.y = jump_speed
+		velocity.y = (jump_speed * FPS_DELTA)
 	#endregion
 	
-	if !on_floor():
+	if !on_floor:
 		velocity.y += gravity * delta
-	elif current_jump_time <= 0:
+	elif !is_jumping:
 		velocity.y = 0
 
-	if on_floor() && slide_input:
+	if on_floor && slide_input:
 		is_sliding = true
 		Slide()
 
 	velocity.x = direction * WALK_SPEED
-	print(velocity, on_floor())
-	var collision = move_and_collide(velocity * delta, true)
+	var collision = move_and_collide(velocity, true)
 	if collision:
-		#HANDLE COLISION#
+		handle_collision(collision)
 		pass
-	move_and_collide(velocity * delta)
+	
+	on_floor = on_floorUpdate()
+	
+	print(velocity, on_floor ," | ", current_coyote_time)
+	move_and_collide(velocity)
 	return
 
-func on_floor():
+func on_floorUpdate():
 	var collision = move_and_collide(Vector2(0, 1), true)
+	
 	if collision && collision.get_normal().y < 0:
+		current_coyote_time = coyote_max_time
+		fix_velocity_angle(collision)
 		return true
+	else:
+		current_coyote_time -= 1
+	
+	if current_coyote_time > 0:
+		return true
+	
 	return false
 
-func get_collision_type(_collision : KinematicCollision2D):
+func fix_velocity_angle(_collision : KinematicCollision2D):
+	
+	var _velocity = velocity
+	_velocity.y *= -1
+	_velocity = _velocity.rotated(_collision.get_angle())
+	_velocity.y *= -1
+	velocity = _velocity
+	return
+
+func handle_collision(_collision : KinematicCollision2D):
 	
 	return 
 
