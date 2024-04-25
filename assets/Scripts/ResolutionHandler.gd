f98622a523a386f65683b35b85e688a635e2bf6e
Omegalolchan <pedrolucasrochap+git@gmail.com>
2024-04-24 22:03:18 -0300

    Resolution handler done... probably

--- assets/Scripts/ResolutionHandler.gd
+++ assets/Scripts/ResolutionHandler.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name ResolutionHandler
 
-static var scale_factor : int = 6
+static var scale_factor : int = 1
 static var window : Window
 
 func _ready():
@@ -9,6 +9,6 @@ func _ready():
 	window.size_changed.connect(change_resolution)
 
 func change_resolution():
-	#window.size = Vector2i(320, 180) * scale_factor
+	window.size *= scale_factor
 	DisplayServer.window_set_size(window.size,0)
 	return
