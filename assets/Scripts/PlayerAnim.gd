855c569eb8e11f495def99aa88d5856ea0cb8fbb
Omegalolchan <pedrolucasrochap+git@gmail.com>
2024-07-16 13:53:53 -0300

    almost every animationdone, the only one missing is slide straight

--- assets/Scripts/PlayerAnim.gd
+++ assets/Scripts/PlayerAnim.gd
@@ -1,5 +1,10 @@
 extends AnimatedSprite2D
 
+var hold = [
+	"", # EX : air, jump (all ground anim cancels)
+	0,
+]
+
 enum anim_to_play {
 	IDLE,
 	WALK,
@@ -8,12 +13,17 @@ enum anim_to_play {
 	SLIDE_UP,
 	SLIDE_STRAIGHT,
 	SLIDE_DOWN,
+	FALL,
 }
 
 func _on_player_var_to_anim(ground:bool, speed:Vector2, jump:bool, wall_jump:bool, walled:bool, slide:bool):
-	if speed.x != 0:
+	if speed.x != 0 and animation != "WALLJUMP":
 		scale.x = sign(speed.x)
 	
+	if hold[1] >= frame:
+		hold[1] = 0
+		hold[0] = ""
+
 	if ground:
 		if speed == Vector2.ZERO:
 			play_anim(anim_to_play.IDLE)
@@ -23,6 +33,19 @@ func _on_player_var_to_anim(ground:bool, speed:Vector2, jump:bool, wall_jump:boo
 			return
 		if speed != Vector2.ZERO and slide:
 			play_anim(calc_slide_angle(speed))
+	else:
+		if speed.y > 0 and hold[0] == "":
+			play_anim(anim_to_play.FALL)
+			return
+		if speed.y <= 0 and jump and hold[0] == "" or hold[0] == "jump":
+			play_anim(anim_to_play.JUMP)
+			return
+		if wall_jump and hold[0] == "" or hold[0] == "jump":
+			play_anim(anim_to_play.WALLJUMP)
+			hold[0] = "jump"
+			hold[1] = 5
+			scale.x = sign(-speed.x)
+			return
 
 
 	pass
